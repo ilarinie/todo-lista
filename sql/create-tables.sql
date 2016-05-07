b27a37e21c6a65c1ecb2440db23f12913e19dbc3
ilarinie <ilari.nieminen@outlook.com>
2016-05-07 19:34:39 +0300

    Update create-tables.sql

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -1,6 +1,7 @@
-CREATE TABLE Tehtavakategoria(
-tehtava_id integer references Tehtava(id),
-kategoria_id integer references Kategoria(id)
+CREATE TABLE Kayttaja(
+id SERIAL PRIMARY KEY,
+nimi varchar(50) NOT NULL,
+salasana varchar(50) NOT NULL
 );
 
 CREATE TABLE Tehtava(
@@ -8,20 +9,19 @@ id SERIAL PRIMARY KEY,
 otsikko varchar(255) NOT NULL,
 kuvaus varchar(255),
 prioriteetti integer,
+suoritettu boolean,
+yksityinen boolean,
 kayttaja_id integer REFERENCES Kayttaja(id)
 );
-
-CREATE TABLE Kayttaja(
-id SERIAL PRIMARY KEY,
-nimi varchar(50) NOT NULL,
-salasana varchar(50) NOT NULL
-);
-
-
 
 CREATE TABLE Kategoria(
 id SERIAL PRIMARY KEY,
 otsikko varchar(255)
+);
+
+CREATE TABLE Tehtavakategoria(
+tehtava_id integer references Tehtava(id),
+kategoria_id integer references Kategoria(id)
 );
 
 
