30b85f249be5fbcdefe0893e81718d66d049a405
ilarinie <ilari.nieminen@outlook.com>
2016-05-07 20:26:46 +0300

    Update insert-data.sql

--- sql/insert-data.sql
+++ sql/insert-data.sql
@@ -1,14 +1,14 @@
 INSERT INTO Kayttaja (nimi, salasana) values ('test', 'test');
 INSERT INTO Kayttaja (nimi, salasana) values ('test2', 'test2');
 
-INSERT INTO Kategoria (otsikko) VALUES ('testikategoria');
-INSERT INTO Kategoria (otsikko) VALUES ('testikategoria2');
+INSERT INTO Kategoria (otsikko, kayttaja_id) VALUES ('testikategoria', 1);
+INSERT INTO Kategoria (otsikko, kayttaja_id) VALUES ('testikategoria2', 2);
 
-INSERT INTO Tehtava (otsikko, kuvaus, prioriteetti, kayttaja_id, yksityinen) VALUES
-('testitehtava', 'testailua', 1, 1, true);
+INSERT INTO Tehtava (otsikko, kuvaus, prioriteetti, kayttaja_id) VALUES
+('testitehtava', 'testailua', 1, 1);
 
-INSERT INTO Tehtava (otsikko, kuvaus, prioriteetti, kayttaja_id, yksityinen) VALUES
-('testitehtava2', 'testailua', 4, 2, false);
+INSERT INTO Tehtava (otsikko, kuvaus, prioriteetti, kayttaja_id) VALUES
+('testitehtava2', 'testailua', 4, 2);
 
 INSERT INTO Tehtavakategoria (tehtava_id, kategoria_id) VALUES (1,1);
 INSERT INTO Tehtavakategoria (tehtava_id, kategoria_id) VALUES (1,2);
